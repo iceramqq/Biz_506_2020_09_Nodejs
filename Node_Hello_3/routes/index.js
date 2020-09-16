ca8dcf81b84108bae13cdf85eeaeaff93d4dade2
iceramqq <iceram22@naver.com>
2020-09-16 15:25:11 +0900

    nodejs

--- Node_Hello_3/routes/index.js
+++ Node_Hello_3/routes/index.js
@@ -5,7 +5,8 @@ const moment = require("moment");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   let date = moment(new Date()).format("YYYY-MM-DD");
-  res.render("index", { title: "반갑습니다", date: date });
+  let time = moment(new Date()).format("HH:mm:ss");
+  res.render("index", { title: "반갑습니다", date: date, time: time });
 });
 
 module.exports = router;
